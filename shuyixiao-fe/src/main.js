84387a6b290ba4ae35afaf9a13a0890883d1dc07
ShuYixiao <chinasjh2022@126.com>
2025-03-27 13:52:29 +0800

    :sparkles: 用户登录前台功能实现

--- shuyixiao-fe/src/main.js
+++ shuyixiao-fe/src/main.js
@@ -5,6 +5,7 @@ import './style/index.css'
 import './style.css'
 import App from './App.vue'
 import router from "./router/index";
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 // 根路径+接口地址
 // axios.get();
@@ -12,3 +13,6 @@ const app = createApp(App)
 app.use(router);
 app.use(ElementPlus);
 app.mount('#app')
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
--- shuyixiao-fe/src/views/Login.vue
+++ shuyixiao-fe/src/views/Login.vue
@@ -2,15 +2,15 @@
   <div class="login-wrapper">
     <div class="modal-content">
       <el-form>
-        <div class="title">一笑管理系统</div>
+        <div class="title">舒一笑系统</div>
         <el-form-item>
-          <el-input type="text" placeholder="用户名"/>
+          <el-input type="text" placeholder="用户名称" prefix-icon="User"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-input type="password" placeholder="密码"/>
+          <el-input type="password" placeholder="用户密码" prefix-icon="View"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" class="login-btn">登录</el-button>
+          <el-button type="primary" class="login-btn">登录按钮</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -29,8 +29,24 @@
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 100%;
+  height: 100vh;
   background-color: #888888;
+}
+.modal-content{
+  width: 500px;
+  padding: 50px;
+  background-color: #ffffff;
+  border-radius: 8px;
+  box-shadow: 0 0 10px 3px #999;
+}
+.title{
+  font-size: 40px;
+  line-height: 1.5;
+  margin-bottom: 30px;
+}
+.login-btn{
+  width: 100%;
+  height: 40px;
 }
 
 </style>
