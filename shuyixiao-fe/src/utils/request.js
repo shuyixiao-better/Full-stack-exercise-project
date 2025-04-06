6416facfc803b320f79085ba3fda7c0116cb8de6
ShuYixiao <chinasjh2022@126.com>
2025-04-06 23:07:17 +0800

    :sparkles: token请求全局拦截优化

--- shuyixiao-fe/src/utils/request.js
+++ shuyixiao-fe/src/utils/request.js
@@ -16,30 +16,65 @@ instance.interceptors.request.use(function (config) {
     // 在发送请求之前做些什么
     const headers = config.headers;
     if(storage.getItem('userInfo')){
-        const {token} = storage.getItem('userInfo').data || {token:""};
-        if (!headers.Authorization) headers.Authorization = "Bearer " + token; //jwt token
+        console.log("141251"+storage.getItem('userInfo').data)
+        const {accessToken} = storage.getItem('userInfo').data || {token:""};
+        if (!headers.Authorization) headers.Authorization = "Bearer " + accessToken; //jwt token
     }
     return config;
 });
 
 //响应拦截
-instance.interceptors.response.use(function (response) {
-    console.log(response)
-    const { code, msg } = response.data; //状态码  接口  HTTP
-    if (code === 200) {
-        response.data;
-    } else if (code === 50001) {
-        ElMessage.error(msg);
-        setTimeout(() => {
-            router.push("/login");
-        }, 3000);
-        return Promise.reject(msg);
-    } else {
-        console.log(33333)
-        ElMessage.error(msg || NETWORK_ERROR);
+// instance.interceptors.response.use(function (response) {
+//     const { code, msg } = response.data; //状态码  接口  HTTP
+//     if (code === 200) {
+//         response.data;
+//     } else if (code === 50001) {
+//         ElMessage.error(msg);
+//         setTimeout(() => {
+//             router.push("/login");
+//         }, 3000);
+//         return Promise.reject(msg);
+//     } else {
+//         ElMessage.error(msg || NETWORK_ERROR);
+//     }
+//     return response;
+// });
+instance.interceptors.response.use(
+    function (response) {
+        // 处理2xx状态码响应
+        const { code, msg } = response.data;
+        if (code === 200) {
+            // response.data;
+            return response;
+        } else if (code === 50001) {
+            ElMessage.error(msg);
+            setTimeout(() => {
+                router.push("/login");
+            }, 300);
+            return Promise.reject(msg);
+        } else {
+            ElMessage.error(msg || NETWORK_ERROR);
+        }
+        return response;
+    },
+    function (error) {
+        // 处理非2xx状态码响应
+        if (error.response && error.response.data) {
+            const { msg, code } = error.response.data;
+            if (code === 401) {
+                ElMessage.error(msg || "认证失败");
+                setTimeout(() => {
+                    router.push("/login");
+                }, 3000);
+            } else {
+                ElMessage.error(msg || NETWORK_ERROR);
+            }
+        } else {
+            ElMessage.error(NETWORK_ERROR);
+        }
+        return Promise.reject(error);
     }
-    return response;
-});
+);
 
 function request(options) {
     options.method = options.method || "get";
